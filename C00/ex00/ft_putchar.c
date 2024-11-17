9b8085095b97ef83277d56b4b0b9527880939be3
Charly692 <mohamed@ubuntu-mohamed>
2024-11-17 02:01:04 +0100

    secondtry

---
+++ C00/ex00/ft_putchar.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+#include <unistd.h>
+
+
+
+void ft_putchar(char c)
+{
+  write(1, &c, 1);
+  write(1, "\n", 1);
+}
+/*
+int main(void)
+{
+  ft_putchar('a');
+}
+*/
---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+  char c;
+  c = 'a';
+
+  while(c != 'z')
+  {
+    c++;
+    write(1, &c, 1);    
+  }
+  write(1, "\n", 1);
+}
+
+int main()
+{
+  ft_print_alphabet();
+}
